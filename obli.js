c7d14bb1f02a92e97c2d4ed62c8728d6c135c6f1
Fausto <fautitoysb@gmail.com>
2024-07-08 18:08:39 -0300

    Add files via upload

---
+++ obli.js
@@ -0,0 +1,9 @@
+
+
+function tomarDatosDeTabla (){
+
+    let nombre = document.getElementById('nombre').value;
+    let tipo = document.getElementById('tipo').value;
+    let temporada = document.getElementById('temporada').value;
+    let marca = document.getElementById('marca').value;
+}
